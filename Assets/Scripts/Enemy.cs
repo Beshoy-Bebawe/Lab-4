964ade8b7e344e175695cd478141f4e32462e59c
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-01-19 12:35:31 -0500

    yea

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Rigidbody enemyRb;
+    private GameObject Player;\
+    public float speed 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
+        Player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
