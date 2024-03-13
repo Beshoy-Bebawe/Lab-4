b21496020ad3f1e7dffd26b2ca2b282cf1f77866
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-03-13 12:22:32 -0400

    I did some work on  lab 4 f

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private Rigidbody enemyRb;
-    private GameObject Player;\
-    public float speed 
+    private GameObject Player;
+    public float speed;
+     
     // Start is called before the first frame update
     void Start()
     {
