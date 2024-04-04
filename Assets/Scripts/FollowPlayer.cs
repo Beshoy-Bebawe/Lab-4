bfc4a5664f9eedce2ccfe996d69dfbcb194e7072
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-04-04 12:49:54 -0400

    I did as much as I can

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
- ;
-  public gameObject player;
+ 
+  public GameObject player;
   private Vector3 offset = new Vector3(0, 12 ,1); 
     // Start is called before the first frame update
     void Start()
