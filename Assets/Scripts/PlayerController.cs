82ce47feb426d39d5e2cacb1e2841fa47d3c20b6
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-04-05 12:43:03 -0400

    leavibng

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,13 @@ public class PlayerController : MonoBehaviour
     public int score = 0;
     public float horizontalInput;
     public float  verticalInput;
+
+float mousePos;
+     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,9 +22,16 @@ public class PlayerController : MonoBehaviour
     {
        horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        
-        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
-         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
 
+        Vector2 mousePos = (Vector2)Camera.main.ScreenToWorldPoint(verticalInput.mousePos);
+
+       
+        
+        
+    }
+
+    void FixedUpdate()
+    {
+         rb.velocity = new Vector3(horizontalInput * speed,0,verticalInput * speed);
     }
 }
